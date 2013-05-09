f17b684a290a1c89c980d50b20a660b5783260c0
ichim-david <ichim.david@gmail.com>
2013-05-09 17:02:29 +0200

    Added auto_join field along with registry upgrade step

--- collective/xmpp/core/interfaces.py
+++ collective/xmpp/core/interfaces.py
@@ -6,7 +6,6 @@ from zope.interface import Interface
 from zope.interface import implements
 from zope.viewlet.interfaces import IViewletManager
 
-
 class IProductLayer(Interface):
     """ Marker interface for requests indicating the staralliance.theme
         package has been installed.
@@ -174,6 +173,17 @@ class IXMPPSettings(Interface):
             u"in. Be aware that this is probably a bad idea on "
             u"sites with many users!"),
         default=False,
+    )
+
+    auto_join = schema.List(
+        title=_(u"label_xmpp_auto_join",
+                default=u"Auto-join the following chat rooms"),
+        description=_(
+            u"help_xmpp_auto_join",
+            default=u"Users will automatically subscribe to the selected "
+            u"chat rooms"),
+        value_type=schema.TextLine(required=False),
+        required=False
     )
 
 
