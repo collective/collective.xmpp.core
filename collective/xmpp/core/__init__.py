c662363b756e1244e7c59b598230c80bb26d900a
JC Brand <jc@opkode.com>
2013-08-27 21:00:02 +0200

    Add an observer for Twisted's log so that we can log to console

--- collective/xmpp/core/__init__.py
+++ collective/xmpp/core/__init__.py
@@ -1,3 +1,14 @@
+from twisted.python.log import addObserver
 from zope.i18nmessageid import MessageFactory
+import logging
 
+log = logging.getLogger(__name__)
 messageFactory = MessageFactory('collective.xmpp.core')
+
+
+def observer(data):
+    func = data['isError'] and log.warn or log.info
+    for m in data['message']:
+        func('Twisted: %s' % m)
+
+addObserver(observer)
