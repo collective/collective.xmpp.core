7af8ecf91d76790952b855a0e5f1ba683a1fb574
JC Brand <jc@opkode.com>
2018-03-21 11:16:12 +0100

    Remove unused settings and add two new ones

--- collective/xmpp/core/interfaces.py
+++ collective/xmpp/core/interfaces.py
@@ -70,75 +70,6 @@ class IXMPPSettings(Interface):
     """ Global XMPP settings. This describes records stored in the
         configuration registry and obtainable via plone.registry.
     """
-    xmpp_domain = schema.TextLine(
-        title=_(u"label_xmpp_domain",
-                default=u"XMPP Domain"),
-        description=_(
-            u"help_xmpp_domain",
-            default=u"The domain which the XMPP server will serve."
-            u"This is also the domain under which users are "
-            u"registered. XMPP user ids are made up of the plone "
-            u"username and domain, like this: ${username}@${domain}."),
-        required=True,
-        default=u'localhost',
-    )
-
-    hostname = schema.TextLine(
-        title=_(u"label_server_hostname",
-                default=u"XMPP Server Hostname"),
-        description=_(
-            u"help_server_hostname",
-            default=u"The hostname of the server on which the XMPP server "
-            u"is running. Useful when you are running your XMPP server "
-            u"on the same server, LAN or VPN as your Plone site. "
-            u"Otherwise, keep the same as the XMPP domain."),
-        required=True,
-        default=u'localhost',
-    )
-
-    port = schema.Int(
-        title=_(u"label_server_port",
-                default=u"XMPP Server Port"),
-        description=_(
-            u"help_server_port",
-            default=u"The port number of the XMPP server. Default is 5222."),
-        required=True,
-        default=5222,
-    )
-
-    admin_jid = schema.TextLine(
-        title=_(u"label_xmpp_admin_jid",
-                default=u"XMPP Admin JID"),
-        description=_(
-            u"help_xmpp_admin_jid",
-            default=u"The Jabber ID of an XMPP user with administration "
-            u"rights. Plone uses this user to manage (e.g "
-            u"register/unregister) other XMPP users."),
-        required=True,
-        default=u'admin@localhost',
-    )
-
-    admin_password = schema.Password(
-        title=_(u"label_xmpp_admin_password",
-                default="XMPP Admin Password"),
-        required=True,
-        default=u'admin',
-    )
-
-    auto_register_on_login = schema.Bool(
-        title=_(u"label_xmpp_auto_register_on_login",
-                default=u"Automatically register for XMPP on login"),
-        description=_(
-            u"help_xmpp_auto_register_on_login",
-            default=u"Check this option if you want users to be "
-            u"automatically registered on the XMPP server."
-            u"They will be registered once they visit "
-            u"this Plone site while logged in."
-            u"You can also register users manually on the XMPP "
-            u"user setup page."),
-        default=False,
-    )
-
     auto_subscribe = schema.Bool(
         title=_(u"label_xmpp_auto_subscribe",
                 default=u"Auto-subscribe XMPP users"),
@@ -151,6 +82,25 @@ class IXMPPSettings(Interface):
             u"be confirmed once the user being subscribed to logs "
             u"in. Be aware that this is probably a bad idea on "
             u"sites with many users!"),
+        default=False,
+    )
+
+    bosh_url = schema.TextLine(
+        title=_(u"label_xmpp_bosh_url",
+                default=u"BOSH URL"),
+        description=_(
+            u"help_xmpp_bosh_url",
+            default=u"The BOSH URL as exposed by your XMPP server."),
+        required=True,
+        default=u'http://localhost:5280/http-bind',
+    )
+
+    debug = schema.Bool(
+        title=_(u"label_xmpp_debug",
+                default=u"Debug"),
+        description=_(
+            u"help_xmpp_debug",
+            default=u"Enable debug logging in the browser console."),
         default=False,
     )
 
