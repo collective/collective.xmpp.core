2224109aac24f1407cfea9fa9bddb87b97fe675f
JC Brand <jc@opkode.com>
2018-03-21 16:42:19 +0100

    Bump version number and change indentation

--- setup.py
+++ setup.py
@@ -1,36 +1,34 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.4.dev0'
+version = '1.0.0dev0'
 
-setup(name='collective.xmpp.core',
-      version=version,
-      description="Enables core functionality for XMPP-enabled Plone add-ons.",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      classifiers=[
-        "Development Status :: 4 - Beta",
-        "Framework :: Plone :: 4.1",
-        "Framework :: Plone :: 4.2",
-        "Framework :: Plone :: 4.3",
+setup(
+    name='collective.xmpp.core',
+    version=version,
+    description="Enables core functionality for XMPP-enabled Plone add-ons.",
+    long_description=open("README.txt").read() + "\n" +
+        open(os.path.join("docs", "HISTORY.txt")).read(),
+    classifiers=[
+        "Framework :: Plone :: 5.1",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
-        ],
-      keywords='plone xmpp twisted',
-      author='Plone Community',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='https://github.com/collective/collective.xmpp.core',
-      license='GPL',
-      packages=find_packages(),
-      namespace_packages=['collective', 'collective.xmpp'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=['BeautifulSoup', 'Plone'],
-      extras_require={
-          'test': ['plone.app.testing']
-      },
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    ],
+    keywords='plone xmpp twisted',
+    author='Plone Community',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://github.com/collective/collective.xmpp.core',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['collective', 'collective.xmpp'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['BeautifulSoup', 'Plone'],
+    extras_require={
+        'test': ['plone.app.testing']
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
